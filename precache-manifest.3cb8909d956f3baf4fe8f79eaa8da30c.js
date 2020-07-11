930e50c9492836e72bc4379027484044b35b49c7
Zir <zirteq111@gmail.com>
2020-07-11 20:02:12 +0200

    Updates

---
+++ precache-manifest.3cb8909d956f3baf4fe8f79eaa8da30c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d9c23c1cac1880305cc9beb187766c",
+    "url": "/span-render/index.html"
+  },
+  {
+    "revision": "57f17d2ff50c60ecd882",
+    "url": "/span-render/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5224948d8a5de7920e0d",
+    "url": "/span-render/static/js/2.12f6df05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/span-render/static/js/2.12f6df05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f17d2ff50c60ecd882",
+    "url": "/span-render/static/js/main.2e92281c.chunk.js"
+  },
+  {
+    "revision": "7d6569a88e0f9c8bd615",
+    "url": "/span-render/static/js/runtime-main.a24298b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/span-render/static/media/logo.5d5d9eef.svg"
+  }
+]);
