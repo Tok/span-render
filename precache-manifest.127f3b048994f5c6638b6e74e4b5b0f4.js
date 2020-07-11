3d2bce01f8d204119391f3a3a8bf2683ac24e454
Zir <zirteq111@gmail.com>
2020-07-11 20:28:20 +0200

    Updates

---
+++ precache-manifest.127f3b048994f5c6638b6e74e4b5b0f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fedafc506f9d20638a49ab15af9d7d7",
+    "url": "/span-render/index.html"
+  },
+  {
+    "revision": "7ab70a3dbaa36a81c95e",
+    "url": "/span-render/static/css/main.7c02628d.chunk.css"
+  },
+  {
+    "revision": "9e835f62f07c71c3faba",
+    "url": "/span-render/static/js/2.cad41f4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/span-render/static/js/2.cad41f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab70a3dbaa36a81c95e",
+    "url": "/span-render/static/js/main.d7568c23.chunk.js"
+  },
+  {
+    "revision": "7d6569a88e0f9c8bd615",
+    "url": "/span-render/static/js/runtime-main.a24298b5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/span-render/precache-manifest.3cb8909d956f3baf4fe8f79eaa8da30c.js"
+  "/span-render/precache-manifest.127f3b048994f5c6638b6e74e4b5b0f4.js"
 );
 
 self.addEventListener('message', (event) => {
